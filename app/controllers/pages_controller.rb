a560bd23b79ddcf2d2d649e810c8530ae50ee06a
paulinelemalefan <pauline.lemalefan@gmail.com>
2018-06-17 13:58:30 +0200

    nouveau commit pour mettre à jour les nouveaux fichiers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
+  end
+
+  def team
+    @members = ["Arnaud", "Pauline", "Mycat"]
+  end
+
+  def contact
+    render plain: "Contactez-nous"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/team" => "pages#team"
+  get "/contact" => "pages#contact"
+  get "/products" => "products#index"
+
 end
